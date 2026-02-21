fbeaeb955f347882f4dcbd2f7e2825c225e619b8
GamerMina <qiwiqiwi123456@gmail.com>
2026-02-21 14:48:46 +0500

    add types

--- types/types.go
+++ types/types.go
@@ -27,8 +27,25 @@ type Card struct {
 type Currrency string
 type Money int64
 type PAN string
+type PaymentCategory string
+type PaymentStatus string
+type Phone string
 
 type Payment struct {
-	Id     int
-	Amount Money
+	ID        string
+	AccountID int
+	Amount    Money
+	Category  PaymentCategory
+	Status    PaymentStatus
 }
+type Account struct {
+	ID      int64
+	Phone   Phone
+	Balance Money
+}
+
+const (
+	PaymentStatsOk         PaymentStatus = "OK"
+	PaymentStatsFail       PaymentStatus = "Fail"
+	PaymentStatsInProgress PaymentStatus = "InProgress"
+)
